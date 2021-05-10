58b01f56c92e1313657f3aebc3d09ce85d36bd5a
Rafael Chacin <rafaelchq@gmail.com>
2021-05-10 23:51:48 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MapContainer from "./components/MapContainer/MapContainer";
+
+function App() {
+  return (
+      <div className="App">
+        <MapContainer />
+      </div>
+  );
+}
+
+export default App;
