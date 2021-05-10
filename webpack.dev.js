76557298037fbff9992834a7640ee11b33cea359
Rafael Chacin <rafaelchq@gmail.com>
2021-05-11 00:04:04 +0200

    commit

--- webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-    devServer: {
-        contentBase: './dist',
-    }
-})
